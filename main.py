09cf370a46544ee3f9293cf054f2da67c58545f3
Rotem Brillant <halorotem@gmail.com>
2023-02-05 23:16:00 +0200

    check input

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import threading
+import sys
+import socket
 
 
-# Press the green button in the gutter to run the script.
+def check_input():
+    if len(sys.argv) != 2:
+        print("didn't get thread pool size")
+        return
+    try:
+        thread_pool = int(sys.argv[1])
+    except ValueError:
+        print("thread pool is not a number, please enter a number between 5-20")
+        return
+    if thread_pool <= 0:
+        print("thread pool must be a number between 5-20")
+    return thread_pool
+
+def start_server(thread_pool, listen_port):
+    pass
+
+
+# when server starts, he receives max requests he can process at a time.
+# main opens a listening port.
+# if a connection is received, server checks if it does not exceed max requests
+# if so create a thread that handles that connection.
+# The amount of threads currently at work needs to be a global variable that can be accessed (with mutex) by all the
+# working threads so that they can update it when they are finished with their task
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    thread_pool = check_input()
+    start_server(thread_pool, 2000)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
